66f607f7663337f8e7c16e504caeeb6563a9a553
mldvPaul <mldvPaul@gmail.com>
2023-11-02 11:41:16 +0100

    app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from './components/Todo';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-gray-900 to-gray-600 text-white`
+}
 
 function App() {
+
+  const [todos, setTodos] = useState(['Learn React', 'Learn Tailwind'])
+
   return (
-    <div className="App">
-     ToDo
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder='Add Todo' />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+          
+        </ul>
+      </div>
     </div>
   );
 }
