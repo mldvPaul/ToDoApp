54a5875b3cb5d22cb2a68ced0d78d31022f0d859
mldvPaul <mldvPaul@gmail.com>
2023-11-02 14:12:18 +0100

    read from firebase

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 
 apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-projectId: process.env.REACT_APPFIREBASE_PROJECT_ID,
+projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
 appId: process.env.REACT_APP_APP_ID
@@ -18,5 +18,5 @@ appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
